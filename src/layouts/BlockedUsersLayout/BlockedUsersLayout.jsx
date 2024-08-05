0cb6e5c112fdae16769d0052787d80e0b4cd5776
Daniil Ivanov <ivanovda010101@gmail.com>
2024-08-05 16:19:17 +0300

    ADMIN: manage conflict

--- src/layouts/BlockedUsersLayout/BlockedUsersLayout.jsx
+++ src/layouts/BlockedUsersLayout/BlockedUsersLayout.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import './BlockedUsersLayout.css'; // Импортируем стили
+import './BlockedUsersLayout.css';
+import AddNewUser from "../../components/AddNewUser/AddNewUser";
 
 function BlockedUsersLayout() {
     const [blockedUsers, setBlockedUsers] = useState([]);
-    const [selectedUserId, setSelectedUserId] = useState(null);
+    const [selectedUser, setSelectedUser] = useState(null);
     const token = localStorage.getItem("token");
 
     const fetchBlockedUsers = useCallback(async () => {
@@ -11,16 +12,17 @@ function BlockedUsersLayout() {
             const response = await fetch(`${process.env.REACT_APP_BACKEND_API_ADDRESS}/blockedUnits`, {
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": `${token}`
+                    "Authorization": token
                 },
             });
 
             if (!response.ok) {
+                const errorData = await response.json();
+                console.log(errorData);
                 throw new Error("Ошибка при получении списка заблокированных пользователей");
             }
 
             const data = await response.json();
-            console.log("Fetched blocked users:", data); // Добавляем отладочный лог
             setBlockedUsers(data);
         } catch (error) {
             console.error("Ошибка при получении данных:", error);
@@ -31,8 +33,8 @@ function BlockedUsersLayout() {
         fetchBlockedUsers();
     }, [fetchBlockedUsers]);
 
-    const handleUserClick = (userId) => {
-        setSelectedUserId(prevUserId => prevUserId === userId ? null : userId);
+    const handleUserClick = (user) => {
+        setSelectedUser(selectedUser?.id === user.id ? null : user);
     };
 
     return (
@@ -40,19 +42,34 @@ function BlockedUsersLayout() {
             <h1>Список заблокированных пользователей</h1>
             {blockedUsers.length > 0 ? (
                 <ul className="user-list">
+
                     {blockedUsers.map(user => (
-                        <li key={user.id} className="user-list-item" onClick={() => handleUserClick(user.id)}>
-                            <span>{user.fio}</span>
-                            <div className={`user-details ${selectedUserId === user.id ? 'show' : ''}`}>
-                                <p><strong>Компания:</strong> {user.company.name}</p>
-                                <p><strong>Причина:</strong> {user.reason}</p>
-                                <p><strong>Дата добавления:</strong> {new Date(user.date_add_to_list).toLocaleString()}</p>
-                                <h4>Паспортные данные:</h4>
-                                <p><strong>Серия паспорта:</strong> {user.passports.passport_seria}</p>
-                                <p><strong>Номер паспорта:</strong> {user.passports.passport_number}</p>
-                                <p><strong>Старая серия паспорта:</strong> {user.passports.old_passport_seria}</p>
-                                <p><strong>Старый номер паспорта:</strong> {user.passports.old_passport_number}</p>
+                        <li key={user.id} className="user-list-item" onClick={() => handleUserClick(user)}>
+
+                            <div className="user-list-item-header">
+                                {user.fio}
                             </div>
+
+                            {selectedUser?.id === user.id && (
+                                <div className="user-details">
+                                    <h3>{selectedUser.fio}</h3>
+                                    <p><strong>Компания:</strong> {selectedUser.company.name}</p>
+                                    <p><strong>Причина:</strong> {selectedUser.reason}</p>
+                                    <p><strong>Дата
+                                        добавления:</strong> {new Date(selectedUser.date_add_to_list).toLocaleString()}
+                                    </p>
+                                    <h4>Паспортные данные:</h4>
+                                    <p><strong>Серия паспорта:</strong> {selectedUser.passports.passport_seria}</p>
+                                    <p><strong>Номер паспорта:</strong> {selectedUser.passports.passport_number}</p>
+                                    <p><strong>Старая серия
+                                        паспорта:</strong> {selectedUser.passports.old_passport_seria}</p>
+                                    <p><strong>Старый номер
+                                        паспорта:</strong> {selectedUser.passports.old_passport_number}</p>
+                                    <div className='delete__user'>Удалить</div>
+                                    <div className='edit__user'>Редактировать</div>
+                                </div>
+
+                            )}
                         </li>
                     ))}
                 </ul>
