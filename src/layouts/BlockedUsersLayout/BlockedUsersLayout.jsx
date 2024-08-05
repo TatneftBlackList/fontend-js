72cad6d06f898ba8cba97b04cbe702015ef7781b
Mekan777-alt <mekan2678@gmail.com>
2024-08-05 12:27:45 +0300

    homePage: Update css and new array to blocked units

--- src/layouts/BlockedUsersLayout/BlockedUsersLayout.jsx
+++ src/layouts/BlockedUsersLayout/BlockedUsersLayout.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import './BlockedUsersLayout.css';
+import './BlockedUsersLayout.css'; // Импортируем стили
 
 function BlockedUsersLayout() {
     const [blockedUsers, setBlockedUsers] = useState([]);
-    const [selectedUser, setSelectedUser] = useState(null);
+    const [selectedUserId, setSelectedUserId] = useState(null);
     const token = localStorage.getItem("token");
 
     const fetchBlockedUsers = useCallback(async () => {
@@ -11,16 +11,16 @@ function BlockedUsersLayout() {
             const response = await fetch(`${process.env.REACT_APP_BACKEND_API_ADDRESS}/blockedUnits`, {
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": token
+                    "Authorization": `${token}`
                 },
             });
 
             if (!response.ok) {
-                console.log(response.json())
                 throw new Error("Ошибка при получении списка заблокированных пользователей");
             }
 
             const data = await response.json();
+            console.log("Fetched blocked users:", data); // Добавляем отладочный лог
             setBlockedUsers(data);
         } catch (error) {
             console.error("Ошибка при получении данных:", error);
@@ -31,36 +31,33 @@ function BlockedUsersLayout() {
         fetchBlockedUsers();
     }, [fetchBlockedUsers]);
 
-    const handleUserClick = (user) => {
-        setSelectedUser(user);
+    const handleUserClick = (userId) => {
+        setSelectedUserId(prevUserId => prevUserId === userId ? null : userId);
     };
 
     return (
-        <div>
+        <div className="container">
             <h1>Список заблокированных пользователей</h1>
             {blockedUsers.length > 0 ? (
                 <ul className="user-list">
                     {blockedUsers.map(user => (
-                        <li key={user.id} className="user-list-item" onClick={() => handleUserClick(user)}>
-                            {user.fio}
+                        <li key={user.id} className="user-list-item" onClick={() => handleUserClick(user.id)}>
+                            <span>{user.fio}</span>
+                            <div className={`user-details ${selectedUserId === user.id ? 'show' : ''}`}>
+                                <p><strong>Компания:</strong> {user.company.name}</p>
+                                <p><strong>Причина:</strong> {user.reason}</p>
+                                <p><strong>Дата добавления:</strong> {new Date(user.date_add_to_list).toLocaleString()}</p>
+                                <h4>Паспортные данные:</h4>
+                                <p><strong>Серия паспорта:</strong> {user.passports.passport_seria}</p>
+                                <p><strong>Номер паспорта:</strong> {user.passports.passport_number}</p>
+                                <p><strong>Старая серия паспорта:</strong> {user.passports.old_passport_seria}</p>
+                                <p><strong>Старый номер паспорта:</strong> {user.passports.old_passport_number}</p>
+                            </div>
                         </li>
                     ))}
                 </ul>
             ) : (
                 <p>Заблокированных пользователей нет</p>
-            )}
-            {selectedUser && (
-                <div className="user-details">
-                    <h3>{selectedUser.fio}</h3>
-                    <p><strong>Компания:</strong> {selectedUser.company.name}</p>
-                    <p><strong>Причина:</strong> {selectedUser.reason}</p>
-                    <p><strong>Дата добавления:</strong> {new Date(selectedUser.date_add_to_list).toLocaleString()}</p>
-                    <h4>Паспортные данные:</h4>
-                    <p><strong>Серия паспорта:</strong> {selectedUser.passports.passport_seria}</p>
-                    <p><strong>Номер паспорта:</strong> {selectedUser.passports.passport_number}</p>
-                    <p><strong>Старая серия паспорта:</strong> {selectedUser.passports.old_passport_seria}</p>
-                    <p><strong>Старый номер паспорта:</strong> {selectedUser.passports.old_passport_number}</p>
-                </div>
             )}
         </div>
     );
